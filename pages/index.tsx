d8e973ecdb34ec74700e1a24625f43b0c6e17039
Hana Navratilova <HanaN@kentico.com>
2021-04-15 14:29:15 +0200

    add kontent-delivery

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,3 +1,21 @@
+/* import {
+    ContentItem,
+    Elements,
+    TypeResolver,
+    DeliveryClient
+} from '@kentico/kontent-delivery';
+
+const deliveryClient = new DeliveryClient({
+    projectId: '93665d46-fba8-0130-141c-dbb905222ba6',
+    previewApiKey: 'ew0KICAiYWxnIjogIkhTMjU2IiwNCiAgInR5cCI6ICJKV1QiDQp9.ew0KICAianRpIjogImRlMmQ4NzZmZmYxZjQ3Yzg4YjU4MGY0NjMxYWQxM2Q4IiwNCiAgImlhdCI6ICIxNjE4NDg3NTU5IiwNCiAgImV4cCI6ICIxOTY0MDg3NTU5IiwNCiAgInByb2plY3RfaWQiOiAiOTM2NjVkNDZmYmE4MDEzMDE0MWNkYmI5MDUyMjJiYTYiLA0KICAidmVyIjogIjEuMC4wIiwNCiAgImF1ZCI6ICJwcmV2aWV3LmRlbGl2ZXIua2VudGljb2Nsb3VkLmNvbSINCn0.TNlElGA-Avic70ozYTgfmRBi0-VAuQmKQ2KsTFMJ2Xc',
+    globalQueryConfig: {
+        usePreviewMode: true,
+    },
+    typeResolvers: [
+        new TypeResolver()
+    ]
+}); */
+
 const Index = () => <p>index</p>;
 
 export default Index;

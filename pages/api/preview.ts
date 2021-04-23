391d88d9666a71d66c2797ad0b4d688f93dc2113
PetrSvirak <PetrS2@kentico.com>
2021-04-23 15:36:13 +0200

    Remove preview obfuscation in favor of env. secret
    The secret value was previously exposed via open source
    GitHub repository. Newly, it is a part of Vercel
    environment and Kontent preview settings (where it can
    actually be read from).

--- pages/api/preview.ts
+++ pages/api/preview.ts
@@ -2,7 +2,11 @@ const urlRegEx = new RegExp("^[a-z-]+:", "i");
 const isRelative = (url: string) => !urlRegEx.test(url);
 
 export default (req, res) => {
-  const redirectTo = req.query.as;
+  const { as: redirectTo, ps: previewSecret } = req.query;
+
+  if (previewSecret != process.env.previewSecret) {
+    return res.status(404).json({ message: "Malformed request url" });
+  }
 
   if (!isRelative(redirectTo)) {
     return res.status(404).json({ message: "Malformed request url" });

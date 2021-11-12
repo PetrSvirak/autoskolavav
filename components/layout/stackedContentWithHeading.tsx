5fd20124f7420702a9ce342619510baf101e4052
Jan Kalfus <JanK@kentico.com>
2021-11-12 15:05:08 +0100

    Make content take as much space as possible, up to 800px

--- components/layout/stackedContentWithHeading.tsx
+++ components/layout/stackedContentWithHeading.tsx
@@ -7,13 +7,15 @@ export const StackedContentWithHeading: React.FC<{ pageName: string }> = ({
   pageName,
   children,
 }) => (
-  <Box maxW="site" padding="4">
-    <ContentHead pageName={pageName} />
-    <Stack spacing={8}>
-      <Heading size={Size.H1} type={HeadingType.Secondary}>
-        {pageName}
-      </Heading>
-      {children}
-    </Stack>
+  <Box width="100%">
+    <Box margin="0 auto" maxW="site" padding="4">
+      <ContentHead pageName={pageName} />
+      <Stack spacing={8}>
+        <Heading size={Size.H1} type={HeadingType.Secondary}>
+          {pageName}
+        </Heading>
+        {children}
+      </Stack>
+    </Box>
   </Box>
 );

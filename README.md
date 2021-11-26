4aa12a8bbfb4cc6946b93ba072133f190431e82f
PetrSvirak <PetrS2@kentico.com>
2021-11-26 10:27:32 +0100

    Prettify files and update README.md
    I set line length so 120 characters - same as is the "hard wrap" settings in
    WebStorm.

--- README.md
+++ README.md
@@ -2,13 +2,24 @@ This is a [Next.js](https://nextjs.org/) project bootstrapped with [`create-next
 
 ## Getting Started
 
-First, run the development server:
+First, make sure you have your **_.env.local_** file ready. It needs
+
+- projectId
+- (optional) previewSecret
+- (optional) previewApiKey
+
+Second, run the development server:
 
 ```bash
 npm run dev
 # or
 yarn dev
 ```
+
+Before commit:
+
+- run either `check-format` or `format`.
+- test your change (there are no automated tests in this repository)
 
 Open [http://localhost:3000](http://localhost:3000) with your browser to see the result.
 
--- utilities/catchEmAllStatic.ts
+++ utilities/catchEmAllStatic.ts
@@ -1,15 +1,17 @@
 import { GetStaticPropsResult } from "next";
 
-export const catchEmAllStatic = <T>(
-  getter: () => Promise<GetStaticPropsResult<T>>
-): (() => Promise<GetStaticPropsResult<T>>) => async () => {
-  try {
-    return await getter();
-  } catch (e) {
-    console.error(e);
+export const catchEmAllStatic =
+  <T>(
+    getter: () => Promise<GetStaticPropsResult<T>>
+  ): (() => Promise<GetStaticPropsResult<T>>) =>
+  async () => {
+    try {
+      return await getter();
+    } catch (e) {
+      console.error(e);
 
-    return {
-      notFound: true,
-    };
-  }
-};
+      return {
+        notFound: true,
+      };
+    }
+  };

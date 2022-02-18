dbc696cf9f9f75b08aa8b4bc37b5257445827e1a
Jan Kalfus <JanK@kentico.com>
2022-02-18 16:00:58 +0100

    Improve mobile menu and logo display - quick fix

--- components/layout/siteHeading.tsx
+++ components/layout/siteHeading.tsx
@@ -1,4 +1,5 @@
 import {
+  Box,
   Container,
   Flex,
   Heading,
@@ -6,23 +7,48 @@ import {
   Spacer,
 } from "@chakra-ui/react";
 import { Menu } from "./menu";
+import { useShouldShowMobileMenu } from "../../hooks/useShouldShowMobileMenu";
 
-const Logo = () => (
-  <ChakraLink href="/" textStyle="link" _hover={{ textDecoration: "none" }}>
-    <Heading as="h1" textAlign="center">
-      <Container textStyle="logo-head">V&V</Container>
-      <Container textStyle="logo-tail">Autoškola</Container>
-      <Container textStyle="logo-preview">
-        {process.env.isPreviewSite && "👁"}
-      </Container>
-    </Heading>
-  </ChakraLink>
-);
+const Logo = () => {
+  const isMobile = useShouldShowMobileMenu();
+  return (
+    <ChakraLink href="/" textStyle="link" _hover={{ textDecoration: "none" }}>
+      <Heading as="h1" p={isMobile ? 0 : undefined} textAlign="center">
+        <Container textStyle="logo-head" fontSize={isMobile ? 48 : undefined}>
+          V&V
+        </Container>
+        <Container textStyle="logo-tail" fontSize={isMobile ? 14 : undefined}>
+          Autoškola
+        </Container>
+        <Container
+          textStyle="logo-preview"
+          fontSize={isMobile ? 16 : undefined}
+        >
+          {process.env.isPreviewSite && "👁"}
+        </Container>
+      </Heading>
+    </ChakraLink>
+  );
+};
 
-export const SiteHeading = () => (
-  <Flex maxWidth="site" padding="26px" width="100%">
-    <Logo />
-    <Spacer />
-    <Menu />
-  </Flex>
-);
+export const SiteHeading = () => {
+  const isMobile = useShouldShowMobileMenu();
+
+  const desktopPadding = "26px";
+
+  return (
+    <Flex
+      maxWidth="site"
+      pb={isMobile ? 0 : desktopPadding}
+      pt={isMobile ? 4 : desktopPadding}
+      px={desktopPadding}
+      width="100%"
+    >
+      <Logo />
+      <Spacer />
+      <Box pt={isMobile ? 4 : undefined}>
+        <Menu />
+      </Box>
+    </Flex>
+  );
+};
